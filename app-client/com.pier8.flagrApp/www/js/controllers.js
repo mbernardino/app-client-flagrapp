c0b7f2824e59bccf44f821e582e7b52c85cf5f57
moises_cit <moises@ciandt.com>
2015-08-20 12:56:45 -0300

    seg teste

--- app-client/com.pier8.flagrApp/www/js/controllers.js
+++ app-client/com.pier8.flagrApp/www/js/controllers.js
@@ -1,125 +1,39 @@
-'use strict';
+var app = angular.module("app", []);
 
-/* Controllers */
-function HomeCtrl($scope,navSvc,$rootScope) {
-    $rootScope.showSettings = false;
-    $scope.slidePage = function (path,type) {
-        navSvc.slidePage(path,type);
-    };
-    $scope.back = function () {
-        navSvc.back();
-    };
-    $scope.changeSettings = function () {
-        $rootScope.showSettings = true;
-    };
-    $scope.closeOverlay = function () {
-        $rootScope.showSettings = false;
-    };
-}
+app.directive('camera', function() {
+	   return {
+	      restrict: 'A',
+	      require: 'ngModel',
+	      link: function(scope, elm, attrs, ctrl) {
 
-function NotificationCtrl($scope) {
-    $scope.alertNotify = function() {
-        navigator.notification.alert("Sample Alert",function() {console.log("Alert success")},"My Alert","Close");
-    };
-    
-    $scope.beepNotify = function() {
-        navigator.notification.beep(1);
-    };
-    
-    $scope.vibrateNotify = function() {
-        navigator.notification.vibrate(3000);
-    };
-    
-    $scope.confirmNotify = function() {
-        navigator.notification.confirm("My Confirmation",function(){console.log("Confirm Success")},"Are you sure?",["Ok","Cancel"]);
-    };
-}
+		 elm.on('click', function() {
+		    navigator.camera.getPicture(function (imageURI) {
+		       scope.$apply(function() {
+			  ctrl.$setViewValue(imageURI);
+		       });
+		    }, function (err) {
+		       ctrl.$setValidity('error', false);
+		    }, { 
+			quality : 50,
+			destinationType : Camera.DestinationType.DATA_URL,
+			sourceType : Camera.PictureSourceType.PHOTOLIBRARY,
+			allowEdit : true,
+			encodingType: Camera.EncodingType.JPEG,
+			targetWidth: 1000,
+			targetHeight: 1000,
+			popoverOptions: CameraPopoverOptions,
+			saveToPhotoAlbum: false 
+		    })
+		 });  
+	      }
+	   };	
+    })
 
-function GeolocationCtrl($scope,navSvc,$rootScope) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        $scope.position=position;
-        $scope.$apply();
-        },function(e) { console.log("Error retrieving position " + e.code + " " + e.message) });
-
-    $scope.back = function () {
-        navSvc.back();
-    };
-}
-
-function AccelerCtrl($scope) {
-    navigator.accelerometer.getCurrentAcceleration(function (acceleration) {
-        $scope.acceleration  = acceleration;
-        },function(e) { console.log("Error finding acceleration " + e) });
-}
-
-function DeviceCtrl($scope) {
-    $scope.device = device;
-}
-
-function CompassCtrl($scope) {
-    navigator.compass.getCurrentHeading(function (heading) {
-        $scope.heading  = heading;
-        $scope.$apply();
-    },function(e) { console.log("Error finding compass " + e.code) });
-}
-
-function HackerNewsCtrl($scope, $rootScope) {
-
-    // load in data from hacker news unless we already have
-    if (!$rootScope.items) {     
-
-        jx.load('http://api.ihackernews.com/page',function(data){
-            console.log(JSON.stringify(data));
-            $rootScope.items = data.items;
-            $scope.$apply();
-        },'json');
-
-    } else {
-        console.log('data already loaded');
-    }
-
-    $scope.loadItem = function(item) {
-        navigator.notification.alert(item.url,function() {console.log("Alert success")},"My Alert","Close");
-    };
-}
-
-
-function ContactsCtrl($scope) {
-    $scope.find = function() {
-        $scope.contacts = [];
-        var options = new ContactFindOptions();
-        //options.filter=""; //returns all results
-        options.filter=$scope.searchTxt;
-        options.multiple=true;
-        var fields = ["displayName", "name", "phoneNumbers"];
-        navigator.contacts.find(fields,function(contacts) {
-            $scope.contacts=contacts;
-            $scope.$apply();
-        },function(e){console.log("Error finding contacts " + e.code)},options);
-    }
-}
-
-function CameraCtrl($scope) {
-    $scope.takePic = function() {
-        var options =   {
-            quality: 50,
-            destinationType: Camera.DestinationType.DATA_URL,
-            sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
-            encodingType: 0     // 0=JPG 1=PNG
-        }
-        // Take picture using device camera and retrieve image as base64-encoded string
-        navigator.camera.getPicture(onSuccess,onFail,options);
-    }
-    var onSuccess = function(imageData) {
-        console.log("On Success! ");
-        $scope.picData = "data:image/jpeg;base64," +imageData;
-        $scope.$apply();
-    };
-    var onFail = function(e) {
-        console.log("On fail " + e);
-    };
-}
-
-
-
-                     
+app.controller("IndexController", function ($scope) {
+    $scope.myPictures = [];
+	$scope.$watch('myPicture', function(value) {
+	   if(value) {
+		  myPictures.push(value);
+	   }
+	}, true);     
+});
